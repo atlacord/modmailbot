69330ff44403e4f7202caef29a06c329a508939c
Kyle <kspa@vt.edu>
2026-08-16 15:27:42 -0500

    Add privacy policy

--- src/modules/webserverPlugin.js
+++ src/modules/webserverPlugin.js
@@ -1,4 +1,9 @@
-module.exports = ({ config, bot }) => {
+module.exports = ({ config, bot, commands }) => {
   const server = require("./webserver")(bot);
   server.listen(config.port, config.host);
+
+  commands.addInboxServerCommand("privacy_policy_link", "", async (msg, args, thread) => {
+    const url = await utils.getSelfUrl("privacy-policy");
+    utils.postSystemMessageWithFallback(msg.channel, thread, `<${url}>`);
+  });
 };
